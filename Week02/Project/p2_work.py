b645a7aa80aca03524f8d8a31810520355e5a992
Leslie Dees <leslie.dees@duke.edu>
2023-09-09 15:23:18 -0400

    checking for import errors

--- Week02/Project/p2_work.py
+++ Week02/Project/p2_work.py
@@ -4,5 +4,5 @@ sys.path.append('C:/Users/lesli/Documents/Duke/Masters/FinTech545')
 import fin_package
 
 
-data = [1, 2, 3, 4, 4]
+data = [1, 2, 3, 4, 4, 4]
 result = fin_package.kurtosis_and_bias(data)
