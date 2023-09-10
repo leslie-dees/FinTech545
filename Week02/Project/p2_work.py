b0b5d446b3f853e66681ffb79bbc8226db16dead
Leslie Dees <leslie.dees@duke.edu>
2023-09-09 23:16:54 -0400

    finished code for problem 2

--- Week02/Project/p2_work.py
+++ Week02/Project/p2_work.py
@@ -12,7 +12,13 @@ X = data['x']
 y = data['y']
 
 print("Performing OLS")
+print("------------------------------------------------------------")
 fin_package.perform_ols(X, y, True)
 
 print("Performing MLE for Normal Distribution")
+print("------------------------------------------------------------")
 fin_package.optimize_normal_distribution(X, y, True)
+
+print("Performing MLE for t Distribution")
+print("------------------------------------------------------------")
+fin_package.optimize_t_distribution(X, y, True)
