5d41606851ac303e92e84dbb5c14f2ae564d6278
Leslie Dees <leslie.dees@duke.edu>
2023-09-09 22:34:23 -0400

    Added normal distribution MLE

--- fin_package.py
+++ fin_package.py
@@ -1,8 +1,10 @@
-from scipy.stats import moment, skew, kurtosis
+from scipy.stats import moment
 import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import norm, gamma
+from scipy.optimize import minimize
 
 def first4Moments(sample, excess_kurtosis=True):
     # Calculate the raw moments
@@ -41,4 +43,56 @@ def perform_ols(X, y, visualize_error=False):
         plt.ylabel("Frequency")
         plt.show()
     
+    print("Error Vector: ", error_vector)
+    averaged_error_vector = np.mean(error_vector)
+    print("Averaged Error Vector: ", averaged_error_vector)
     return error_vector
+
+def optimize_normal_distribution(X, y, perform_hypothesis_test = False):
+    # Define the likelihood function for the normal distribution
+    def log_likelihood(mean, var, X):
+        # Number of values in X
+        n = len(X)
+        # Adjust x to be centered around the mean
+        adjusted_X = X - mean
+        # Get squared variance
+        var2 = var**2
+        # Calculate log likelihood
+        log_likeli = -(n/2) * np.log(var2 * 2 * np.pi) - np.dot(adjusted_X, adjusted_X) / (2 * var2)
+
+        return -log_likeli
+
+    # Calculate initial guess for mean and standard deviation
+    mean_guess = np.mean(y)
+    std_dev_guess = np.std(y)
+
+    # Initial guess for optimization
+    initial_params = [mean_guess, std_dev_guess]
+
+    # Perform optimization through minimization of log likelihood
+    result = minimize(lambda params: log_likelihood(params[0], params[1], X), initial_params)
+
+    # Extract optimized parameters
+    optimized_mean, optimized_std_dev = result.x
+    
+    # Print optimized mean and standard deviation
+    print("Optimized Mean:", optimized_mean)
+    print("Optimized Standard Deviation:", optimized_std_dev)
+
+    # Perform hypothesis test if specified
+    if perform_hypothesis_test:
+        # Calculate test statistic and p-value against standard normal (0, 1)
+        test_statistic = (optimized_mean - 0) / optimized_std_dev  # Z-score
+        p_value = 2 * (1 - norm.cdf(abs(test_statistic)))  # Two-tailed test
+
+        # Determine if the null hypothesis (X is from a standard normal distribution) is rejected
+        reject_null = p_value < 0.05  # Using a significance level of 0.05
+
+        # Print hypothesis test results
+        print("Test Statistic:", test_statistic)
+        print("P-Value:", p_value)
+        print("Reject Null Hypothesis:", reject_null)
+
+    
+
+    return optimized_mean, optimized_std_dev
