d84ca5e9eb221ee351e72f8717501308e0226d26
Leslie Dees <leslie.dees@duke.edu>
2023-09-09 22:34:23 -0400

    Added normal distribution MLE

--- Week02/Project/p2_work.py
+++ Week02/Project/p2_work.py
@@ -2,8 +2,17 @@ import sys
 sys.path.append('C:/Users/lesli/Documents/Duke/Masters/FinTech545')
 import fin_package
 import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+from scipy.stats import norm, t
 
 # Import data and separate into x and y components
 data = pd.read_csv('Week02/Project/problem2.csv')
 X = data['x']
-Y = data['y']
+y = data['y']
+
+print("Performing OLS")
+fin_package.perform_ols(X, y, True)
+
+print("Performing MLE for Normal Distribution")
+fin_package.optimize_normal_distribution(X, y, True)
